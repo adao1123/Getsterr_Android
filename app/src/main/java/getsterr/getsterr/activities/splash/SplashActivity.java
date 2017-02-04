e4c3a7d989b4c92cb8ad56d68d1f3e320ea3ec97
adao1123 <adao1193@gmail.com>
2017-02-04 11:43:24 -0800

    Splash page will go Set up screen on first time, then will go to dashboard everytime after

--- app/src/main/java/getsterr/getsterr/activities/splash/SplashActivity.java
+++ app/src/main/java/getsterr/getsterr/activities/splash/SplashActivity.java
@@ -1,6 +1,8 @@
 package getsterr.getsterr.activities.splash;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -23,29 +25,51 @@ public class SplashActivity extends AppCompatActivity {
         if (!splashLoaded) {
             setContentView(R.layout.activity_splash);
             splashLayout = (RelativeLayout)findViewById(R.id.splash_layout);
-            splashLayout.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    startActivity(new Intent(SplashActivity.this, DashBoardActivity.class));
-                }
-            });
-            int secondsDelayed = 1;
-//            setFont();
+//            splashLayout.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    if (isFirstTime()) {
+//                        setNotFirstTime();
+//                        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+//                    }else startActivity(new Intent(SplashActivity.this, DashBoardActivity.class));
+//                }
+//            });
+            int secondsDelayed = 3;
 
             new Handler().postDelayed(new Runnable() {
                 public void run() {
-                    startActivity(new Intent(SplashActivity.this, DashBoardActivity.class));
+                    if (isFirstTime()) {
+                        setNotFirstTime();
+                        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                    }else startActivity(new Intent(SplashActivity.this, DashBoardActivity.class));
                     finish();
                 }
-            }, secondsDelayed * 5000);
+            }, secondsDelayed * 1000);
 
             splashLoaded = true;
         }
         else {
-            Intent goToFacebookActivity = new Intent(SplashActivity.this, DashBoardActivity.class);
+            Intent goToFacebookActivity;
+            if (isFirstTime()) {
+                setNotFirstTime();
+                goToFacebookActivity = new Intent(SplashActivity.this, MainActivity.class);
+            }else goToFacebookActivity = new Intent(SplashActivity.this, DashBoardActivity.class);
             goToFacebookActivity.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
             startActivity(goToFacebookActivity);
             finish();
         }
+    }
+
+    private boolean isFirstTime(){
+        SharedPreferences sharedPreferences = getPreferences(Context.MODE_PRIVATE);
+        if (sharedPreferences.getString("FIRST_TIME_KEY","FIRST_TIME").equals("FIRST_TIME")) return true;
+        else return false;
+    }
+
+    private void setNotFirstTime(){
+        SharedPreferences sharedPreferences = getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("FIRST_TIME_KEY","HERE");
+        editor.commit();
     }
 }
